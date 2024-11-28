494f32c4eb8dbce01e186b49bcb60fc35463a226
Usodabada23 <redaba6200@gmail.com>
2024-11-28 14:07:13 +0100

    add suggest list with sort/filter and dynamism

--- js/main.js
+++ js/main.js
@@ -3,117 +3,103 @@ const ninjaKey = "eEo0kAEU9JvRiBQXi9FVZQ==ICbDTBzCCTt9ztaM";
 const unsplashKey = "yHNHJjy_sBl1Vr285tqf0qGyqZ2W43Y0Jmi5U8kzCF8";
 const form = document.getElementById("search-form");
 const input = document.getElementById("city-input");
-const datalist = document.querySelector(".search-container__result");
+const datalist = document.getElementById("searchList");
+const home = document.getElementById("home");
 
 //async fetch function
 
-async function getCordinatesByLocationName(city) {
+let coordinates = async function getCoordinatesByLocationName(city) {
   try {
     const result = await fetch(
       `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${openWeatherkey}`
     );
     if (!result.ok) {
+      datalist.innerHTML = "<p>City not found!</p>";
       throw new Error("City not found");
     }
 
     const data = await result.json();
     if (data.length > 0) {
       const { lat, lon } = data[0];
-      getWeatherLatAndLon(lat, lon);
+      return { lat, lon };
     } else {
       datalist.innerHTML = "<p>City not found!</p>";
     }
   } catch (error) {
     datalist.innerHTML = `<p>Error: ${error.message}</p>`;
   }
-}
-async function getWeatherLatAndLon(lat, lon) {
+};
+let weather = async function getWeatherLatAndLon(lat, lon) {
   try {
     const result = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openWeatherkey}&units=metric`
     );
     if (!result.ok) {
-      throw new Error("Error fetching forecast");
+      datalist.innerHTML = "<p>City not found!</p>";
+      throw new Error("City not found");
     }
 
     const data = await result.json();
-    displayWeatherResult(data);
+    return data;
   } catch (error) {
     datalist.innerHTML = `<p>Error: ${error.message}</p>`;
   }
-}
-async function getCities(city) {
+};
+let cities = async function getCities(city) {
   try {
     const result = await fetch(
       `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${openWeatherkey}`
     );
 
     if (!result.ok) {
+      datalist.innerHTML = "<p>City not found!</p>";
       throw new Error("City not found");
     }
 
-    const data = await result.json();
-    updateCityList(data);
+    let data = await result.json();
+    const lowercaseCity = city.toLowerCase();
+    data = data.filter((item) =>
+      item.name.toLowerCase().startsWith(lowercaseCity)
+    );
+
+    data = data.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    return data;
   } catch (error) {
     console.error("Error fetching city data:", error);
   }
-}
-async function suggestions(city) {
-  let options = {
-    method: "GET",
-    headers: { "x-api-key": ninjaKey },
-  };
-  try {
-    const result = await fetch(
-      `https://api.api-ninjas.com/v1/city?name=${city}`,
-      options
-    );
-    if (!result.ok) {
-      throw new Error("City not found");
-    }
-
-    const data = await result.json();
-    const suggestionsList = document.querySelector(
-      ".search-container__form--suggestions-list"
-    );
-    suggestionsList.innerHTML = "";
-    for (item of data) {
-      const option = document.createElement("li");
-      option.innerHTML = `${item.name},${item.country}`;
-
-      option.addEventListener("click", () => {
-        input.value = `${item.name},${item.country}`;
-        suggestionsList.innerHTML = "";
-      });
-
-      suggestionsList.appendChild(option);
-    }
-  } catch (error) {
-    console.error("Error fetching city data:", error);
-  }
-}
-async function getImageByCity(city) {
+};
+let images = async function getImageByCity(city) {
   try {
     const result = await fetch(
       `https://api.unsplash.com/search/photos?query=${city}&client_id=${unsplashKey}`
     );
+    if (!result.ok) {
+      if (result.status === 404) {
+        throw new Error("City picture not found");
+      }
+      datalist.innerHTML = "<p>City picture not found!</p>";
+      console.error("City picture not found");
+      return null;
+    }
+
     const data = await result.json();
     let arrImg = data.results;
     let rand = Math.floor(Math.random() * 10);
     let url = arrImg[rand].links.download;
-    displayImgByCity(url, city);
-  } catch (error) {}
-}
+    return url;
+  } catch (error) {
+    console.error("Error fetching picture data:", error);
+  }
+};
 
 // data function
-
-function updateCityList(cities) {
+function displayCityList(cities) {
   datalist.innerHTML = "";
 
   if (cities.length === 0) {
-    const option = document.createElement("option");
-    option.value = "No cities found";
-    datalist.appendChild(option);
+    datalist.innerHTML = "No cities found";
   } else {
     for (const city of cities) {
       const option = document.createElement("option");
@@ -143,7 +129,7 @@ function displayWeatherResult(data) {
   const tempDatalist = document.createElement("datalist");
   const h3 = document.createElement("h3");
   h3.classList.add("weather-container__h3");
-  h3.innerHTML = `Temperature in ${data.city.name},${data.city.country}<span>.</span>`;
+  h3.id = "weatherTitle";
   section.appendChild(h3);
   tempDatalist.classList.add("weather-container__result");
   for (let i = 0; i < arr.length; i++) {
@@ -175,6 +161,7 @@ function getMaxMinTempByDate(data) {
   let previousDate = null;
 
   for (let i = 0; i < tempArr.length; i++) {
+    //console.log(new Date(tempArr[i]["date"]).toDateString());
     let dateWithoutHour = tempArr[i]["date"].split(" ")[0];
 
     if (dateWithoutHour === previousDate) {
@@ -209,6 +196,7 @@ function displayImgByCity(url, city) {
   const main = document.querySelector("main");
   const imgSection = document.createElement("section");
   imgSection.classList.add("img-container");
+  imgSection.id = "sectionImg";
   main.appendChild(imgSection);
 
   const img = document.createElement("img");
@@ -216,44 +204,68 @@ function displayImgByCity(url, city) {
   h4.classList.add("img-container__h4");
   h4.innerHTML = `A little picture from ${city}<span>.</span>`;
   img.classList.add("img-container__img");
-  img.src = `${url}`;
+  img.src = `${url}&w=1200&h=800&fit=max`;
   imgSection.appendChild(h4);
   imgSection.appendChild(img);
 }
 
-// listener
-form.addEventListener("submit", (e) => {
+// listeners
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const cityName = input.value.trim();
   if (cityName) {
-    getCities(cityName);
+    const city = await cities(cityName);
+    displayCityList(city);
   }
 });
+input.addEventListener("input", async (e) => {
+  e.preventDefault();
 
-input.addEventListener("input", () => {
+  const cityName = input.value.trim();
+  if (cityName) {
+    const cityList = await cities(cityName);
+    displayCityList(cityList);
+  }
   if (input.value === "") {
     datalist.innerHTML = "";
-    const section = document.querySelector(".weather-container");
+    const section = document.getElementById("weatherResult");
     section.innerHTML = "";
-    const suggestions = document.querySelector(
-      ".search-container__form--suggestions-list"
-    );
-    suggestions.innerHTML = "";
-    const imgSection = document.querySelector(".img-container");
+    const imgSection = document.getElementById("sectionImg");
     if (imgSection) {
       imgSection.remove();
     }
-  } else {
-    suggestions(input.value);
   }
-});
-window.addEventListener("load", () => {
-  input.value = "";
 });
 datalist.addEventListener("click", async (e) => {
   let txt = e.target.innerText;
   let arr = txt.split(", ");
-  await getCordinatesByLocationName(arr[0]);
-  await getImageByCity(arr[0]);
+  const section = document.getElementById("weatherResult");
+  section.innerHTML = "";
+  const imgSection = document.getElementById("sectionImg");
+  if (imgSection) {
+    imgSection.remove();
+  }
+  if (section.innerHTML === "") {
+    const coords = await coordinates(arr[0]);
+    const weatherData = await weather(coords.lat, coords.lon);
+    const url = await images(arr[0]);
+    displayWeatherResult(weatherData);
+    const h3 = document.getElementById("weatherTitle");
+    h3.innerHTML = `Temperature in ${txt}<span>.</span>`;
+
+    if (!url) {
+      if (imgSection) {
+        imgSection.innerHTML = `<p>No picture for ${arr[0]} `;
+      }
+    } else {
+      displayImgByCity(url, arr[0]);
+    }
+
+    input.value = "";
+    datalist.innerHTML = "";
+  }
+});
+home.addEventListener("click", (e) => {
+  location.reload(true);
 });
